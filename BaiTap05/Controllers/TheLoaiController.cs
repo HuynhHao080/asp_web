2f48401b6a95c0aab9ed033f478c73f60f3a75fe
HuynhHao080 <hnh.031204@gmail.com>
2024-09-06 14:39:41 +0700

    "Upload BT05"

---
+++ BaiTap05/Controllers/TheLoaiController.cs
@@ -0,0 +1,26 @@
+﻿using BaiTap05.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTap05.Controllers
+{
+    public class TheLoaiController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Ngay = "Ngày 30";
+            ViewBag.Thang = "Tháng 4";
+            ViewData["Nam"] = "Năm 1945";
+            return View();
+        }
+        public IActionResult Index2()
+        {
+            var theloai = new TheLoaiViewModel
+            {
+                Id = 1822040542,
+                Name = "Huỳnh Nhựt Hào",
+                Status = "Đang học"
+            };
+            return View(theloai);   
+        }
+    }
+}
