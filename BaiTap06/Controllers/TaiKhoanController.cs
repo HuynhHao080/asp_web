06cf54f0fc0cea5bfdd7fe6e1e69a74bff45732b
HuynhHao080 <hnh.031204@gmail.com>
2024-09-09 15:06:53 +0700

    "UploadBT06"

---
+++ BaiTap06/Controllers/TaiKhoanController.cs
@@ -0,0 +1,17 @@
+﻿using BaiTap06.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTap06.Controllers
+{
+    public class TaiKhoanController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult DangKy(TaiKhoanViewModel taiKhoan)
+        {
+            return View(taiKhoan);
+        }
+    }
+}
